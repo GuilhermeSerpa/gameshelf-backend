a2b6b0f889006f1f43fc141bfc794e30cfc6a158
GuilhermeSerpa <guilherme.dev.ms@gmail.com>
2026-08-02 20:22:47 -0300

    chore: estrutura inicial do projeto

---
+++ src/main/java/com/gameshelf/gameshelf_backend/GameshelfBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gameshelf.gameshelf_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameshelfBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameshelfBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gameshelf/gameshelf_backend/GameshelfBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gameshelf.gameshelf_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameshelfBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
